4bbbef985aa0652d59fcbc81136fb9440c5a0141
ltsrad <alex+rad@longterm.io>
2022-09-20 14:15:25 -0700

    [mesh] Add event handlers and bridge filter modification

--- code/mesh.go
+++ code/mesh.go
@@ -11,10 +11,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
 import (
@@ -39,10 +42,112 @@ func initMesh() {
 
 }
 
+type ifaceMacKey struct {
+	iface string
+	mac   string
+}
+
+func getExistingBridgeSet() []ifaceMacKey {
+	//google/nftables is incomplete and does not support custom set key types
+
+	existing := []ifaceMacKey{}
+
+	//nft -j list map inet filter dhcp_access
+	cmd := exec.Command("nft", "-j", "list", "map", "bridge", "filter", "bridge_access")
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println("nft error", err)
+		return existing
+	}
+
+	//jq .nftables[1].map.elem[][0].concat
+	var data map[string]interface{}
+	err = json.Unmarshal(stdout, &data)
+	data2, ok := data["nftables"].([]interface{})
+	if ok != true {
+		log.Fatal("invalid json")
+	}
+	data3, ok := data2[1].(map[string]interface{})
+	data4, ok := data3["map"].(map[string]interface{})
+	data5, ok := data4["elem"].([]interface{})
+	for _, d := range data5 {
+		e, ok := d.([]interface{})
+		f, ok := e[0].(map[string]interface{})
+		g, ok := f["concat"].([]interface{})
+		if ok {
+			iface, ok := g[0].(string)
+			mac, ok := g[1].(string)
+			if ok {
+				existing = append(existing, ifaceMacKey{iface, mac})
+			}
+		}
+	}
+	return existing
+}
+
+func updateBridgeAccess(action string, iface string, mac string) {
+	existingSet := getExistingBridgeSet()
+
+	if action == "add" {
+		//if this iface or mac is in an existing key, remove it
+		for _, e := range existingSet {
+			if e.iface == iface || e.mac == mac {
+				exec.Command("nft", "delete", "element", "bridge", "filter", "bridge_access", "{", e.iface, ".", e.mac, ":", "accept", "}").Run()
+			}
+		}
+
+		exec.Command("nft", "add", "element", "bridge", "filter", "bridge_access", "{", iface, ".", mac, ":", "accept", "}").Run()
+	} else if action == "remove" {
+		for _, e := range existingSet {
+			if e.iface == iface || e.mac == mac {
+				exec.Command("nft", "delete", "element", "bridge", "filter", "bridge_access", "{", e.iface, ".", e.mac, ":", "accept", "}").Run()
+			}
+		}
+	}
+}
+
+type WifiConnectEvent struct {
+	Action string
+	Iface string
+	Mac string
+}
+
+func wifiConnect(w http.ResponseWriter, r *http.Request) {
+	//A device connected. Add the interface to the bridge,
+	// and then add it to the bridge_access nft
+
+	event := WifiConnectEvent{}
+	err := json.NewDecoder(r.Body).Decode(&event)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	updateBridgeAccess("add", event.Iface, event.Mac)
+}
+
+func wifiDisconnect(w http.ResponseWriter, r *http.Request) {
+	//A device connected. Add the interface to the bridge,
+	// and then add it to the bridge_access nft
+
+	event := WifiConnectEvent{}
+	err := json.NewDecoder(r.Body).Decode(&event)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	updateBridgeAccess("remove", event.Iface, event.Mac)
+}
+
+
 func main() {
 	unix_plugin_router := mux.NewRouter().StrictSlash(true)
 
 	unix_plugin_router.HandleFunc("/config", getMeshConfig).Methods("GET")
+
+	//good use case for event bus
+	unix_plugin_router.HandleFunc("/stationConnect", wifiConnect).Methods("PUT")
+	unix_plugin_router.HandleFunc("/stationDisconnect", wifiDisconnect).Methods("PUT")
+
 
 	os.Remove(UNIX_PLUGIN_LISTENER)
 	unixPluginListener, err := net.Listen("unix", UNIX_PLUGIN_LISTENER)
---
+++ mesh_rules.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+# Disable IP forwarding. Not needed with bridge.
+
+sysctl net.ipv4.ip_forward=0
+
+# Drop input
+iptables -P INPUT DROP
+
+# Empty flow table
+conntrack -D
+
+# Enable ARP filter
+sysctl net.ipv4.conf.all.arp_filter=1
+
+# Create the bridge interface
+ip link add name br0 type bridge
+ip link set dev br0 up
+
+# Add the upstream interface to the bridge
+ip link set dev $WANIF master br0
+
+# Clean previous rules
+iptables --flush
+iptables -t nat --flush
+iptables --delete-chain
+iptables -t nat --delete-chain
+
+iptables-legacy --flush
+iptables-legacy -t nat --flush
+iptables-legacy --delete-chain
+iptables-legacy -t nat --delete-chain
+
+nft flush ruleset
+
+nft -f - << EOF
+
+table bridge filter {
+
+  #TBD do we integrate IP address filter here also?
+  # Main thing is that leaf stops MAC spoofing for an assigned devices.
+
+  map bridge_access {
+    type ifname . ether_addr: verdict;
+  }
+
+	chain FORWARD {
+		type filter hook forward priority 0; policy drop;
+    counter log prefix "bridge:in " group 0
+
+    counter iifname . ether saddr vmap @bridge_access
+
+		counter log prefix \ " drop:bridge \" group 1
+	}
+}
+
+
+table inet filter {
+
+  # Dynamic maps of clients, populated by wifid
+  map dhcp_access {
+    type ifname . ether_addr: verdict;
+  }
+
+  map upstream_tcp_port_drop {
+    type inet_service : verdict;
+    elements = {
+      22: drop,
+      80: drop,
+      443: drop,
+      5201: drop
+    }
+  }
+
+  map spr_tcp_port_accept {
+    type inet_service : verdict;
+    elements = {
+      22: accept,
+      80: accept,
+      443: accept,
+      5201: accept
+    }
+  }
+
+
+  chain INPUT {
+    type filter hook input priority 0; policy drop;
+
+    #jump USERDEF_INPUT
+    iif lo counter accept
+    counter jump F_EST_RELATED
+
+    # Mark whether the input came from upstream (wan:in) or local network (lan:in)
+    $(if [ "$WANIF" ]; then echo "iifname $WANIF log prefix \"wan:in \" group 0"; fi)
+    $(if [ "$WANIF" ]; then echo "iifname ne $WANIF log prefix \"lan:in \" group 0"; else echo "log prefix \"lan:in \" group 0"; fi)
+
+    # drop dhcp requests, multicast ports from upstream
+    # When updating lan_udp_accept, updated this list.
+    $(if [ "$WANIF" ]; then echo "iifname $WANIF udp dport {67, 1900, 5353} counter jump DROPLOGINP"; fi)
+
+    # drop ssh, iperf from upstream
+    $(if [ "$WANIF" ]; then echo "counter iifname $WANIF tcp dport vmap @upstream_tcp_port_drop"; fi)
+
+    # Allow ssh, iperf3 from LAN and those not dropped from upstream (see upstream_tcp_port_drop)
+    counter tcp dport vmap @spr_tcp_port_accept
+    counter jump DROPLOGINP
+  }
+
+  chain DROPLOGINP {
+    counter log prefix "drop:input " group 1
+    counter drop
+  }
+
+  chain F_EST_RELATED {
+    ip protocol udp ct state related,established counter accept
+    ip protocol tcp ct state related,established counter accept
+    ip protocol icmp ct state related,established counter accept
+  }
+
+}
+
+EOF
+
+
+# Accept input
+iptables -P INPUT ACCEPT
